575aa2a694e25782370877a99e1d3dfadeaf53db
NICHOLSON Matthew <mnn696@quser22.quest.it.northwestern.edu>
2019-05-07 00:26:08 -0500

    working submission script

--- model_submission_script.sh
+++ model_submission_script.sh
@@ -9,9 +9,7 @@
 module purge all	     ## purge environment modules
 module load netlogo/6.0.1           ## Load modules (unchanged)
 
-cd /software/netlogo/6.0.1
-
-netlogo-headless.sh \
---model /projects/p30553/mmss-thesis/dynamic_model.nlogo \
+/software/netlogo/6.0.1/netlogo-headless.sh \
+--model dynamic_model.nlogo \
 --experiment fixed_dis \
---table /projects/p30553/mmss-thesis/BehaviorSearch_output/fixed_dis.csv
+--table /projects/p30553/mmss-thesis/fixed_dis.csv
